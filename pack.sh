93060c37fa0b250a96520759df2f37710b8ac281
RandomiDn <91882824+RandomiDn@users.noreply.github.com>
2021-10-07 05:57:10 +0700

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 if [ -f $1 ]; then cp $1 Image.gz-dtb ; fi
 rm *.zip
-zip -r9 iDnProject-riva-$(date +%Y%m%d).zip * -x .git README.md *placeholder
+zip -r9 iDnProject-riva-$(date +"%S-%F").zip * -x .git README.md *placeholder
