ba5fbe0f22d33aef007d7c763df014a4e4c557d0
RandomiDn <91882824+RandomiDn@users.noreply.github.com>
2021-10-24 11:42:43 +0700

    update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -10,8 +10,8 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=excalibur
-device.name2=RMX2061
+device.name1=RMX2061
+device.name2=
 supported.versions=10-11
 supported.patchlevels=
 '; } # end properties
